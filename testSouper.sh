259f418d03194571135359ddffcd6a42c76ed903
ajrichins <a.j.richins@live.com>
2023-08-18 15:30:48 -0600

    starting to add testing, refining manual testing tools

--- testSouper.sh
+++ testSouper.sh
@@ -3,8 +3,12 @@ if [ "$1" = "" ]
 then
   echo "need number for opt to be tested"
   exit 1
-souper-check -pretty-print=pdl souperOpts/$1.txt > generatedOpts/$1.mlir
-mlir-opt -mlir-print-op-generic generatedOpts/$1.mlir > generatedOpts/g$1.mlir
-mlir-opt -mlir-print-op-generic test/t$1.mlir > test/gt$1.mlir
-python pdl-opt.py test/gt$1.mlir generatedOpts/g$1.mlir
+else
+  echo "generating opt"
+  souper-check -pretty-print=pdl souperOpts/$1.txt > generatedOpts/$1.mlir
+  mlir-opt -mlir-print-op-generic generatedOpts/$1.mlir > generatedOpts/g$1.mlir
+  mlir-opt -mlir-print-op-generic test/t$1.mlir > test/gt$1.mlir
+  echo "testing opt"
+  python pdl-opt.py test/gt$1.mlir generatedOpts/g$1.mlir
+  echo "done"
 fi
