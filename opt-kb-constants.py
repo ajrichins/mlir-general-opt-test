6f06d275608ab6a69764abb38a5dd7b600bbe2b5
ajrichins <a.j.richins@live.com>
2023-10-17 10:49:33 -0600

    fixing syntax to work

--- opt-kb-constants.py
+++ opt-kb-constants.py
@@ -74,12 +74,12 @@ constantRewriteTemplate=Template('''
     %1 = "pdl.operand"(%0) : (!pdl.type) -> !pdl.value
     %2 = "pdl.operand"(%0) : (!pdl.type) -> !pdl.value
     %3 = "pdl.attribute"() {value = "$kbStr"} : () -> !pdl.attribute
-    %4 = "pdl.operation"(%1, %2, %3, %0) {attributeValueNames = ["kb"], opName = "$opName", operandSegmentSizes = array<i32: 2, 1, 1>} : (!pdl.value, !pdl.value, !pdl.attribute, !pdl.type) -> !pdl.operation
+    %4 = "pdl.operation"(%1, %2, %3, %0) {attributeValueNames = ["kb"], opName = "$opName", operand_segment_sizes = array<i32: 2, 1, 1>} : (!pdl.value, !pdl.value, !pdl.attribute, !pdl.type) -> !pdl.operation
     "pdl.rewrite"(%4) ({
       %5 = "pdl.attribute"() {value = $kbVal : i$width} : () -> !pdl.attribute
-      %6 = "pdl.operation"(%5, %0) {attributeValueNames = ["value"], opName = "arith.constant", operandSegmentSizes = array<i32: 0, 1, 1>} : (!pdl.attribute, !pdl.type) -> !pdl.operation
-      "pdl.replace"(%4, %6) {operandSegmentSizes = array<i32: 1, 1, 0>} : (!pdl.operation, !pdl.operation) -> ()
-    }) {operandSegmentSizes = array<i32: 1, 0>} : (!pdl.operation) -> ()
+      %6 = "pdl.operation"(%5, %0) {attributeValueNames = ["value"], opName = "arith.constant", operand_segment_sizes = array<i32: 0, 1, 1>} : (!pdl.attribute, !pdl.type) -> !pdl.operation
+      "pdl.replace"(%4, %6) {operand_segment_sizes = array<i32: 1, 1, 0>} : (!pdl.operation, !pdl.operation) -> ()
+    }) {operand_segment_sizes = array<i32: 1, 0>} : (!pdl.operation) -> ()
   }) {benefit = 1 : i16, sym_name = "$optName"} : () -> ()
 ''')
 
